90e2e7e3690c32e313323a6879068d09e09389c6
hayness666 <hayness666@gmail.com>
2025-08-16 16:05:40 -0600

    Add 2 new banners

--- src/scripts/squads.js
+++ src/scripts/squads.js
@@ -4,6 +4,10 @@ export const squads = [
         coat: "/assets/squads/knights-of-the-crimson-rose.png"
     },
     {
+        name: "Isle of Misfit Toys",
+        coat: "/assets/squads/isle-of-misfit-toys.png"
+    },
+    {
         name: "Nanook's Guardians",
         coat: "/assets/squads/nanooks-guardians.png"
     },
@@ -11,4 +15,8 @@ export const squads = [
         name: "POLAR's Pillagers",
         coat: "/assets/squads/polars-pillagers.png"
     },
+    {
+        name: "Power Bears",
+        coat: "/assets/squads/power-bears-s1.png"
+    }
 ];
